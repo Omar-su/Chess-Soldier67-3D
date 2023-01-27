185cea2fc385988a1da11ea76c06d759cec7a192
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-01-27 15:32:38 +0100

    The hand feature sound effect

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -6,6 +6,8 @@ public class PlayerTeleport : MonoBehaviour
 {
     public Transform orientation;
     public float teleportRange;
+    public AudioSource audioSource;
+    public float volume = 0.5f;
 
     PlayerMovement playerMovement;
     // Start is called before the first frame update
@@ -19,20 +21,21 @@ public class PlayerTeleport : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.F)) {
             StartCoroutine("Teleport");
+            
         }
         print("position in game : " + transform.position+ " y = " + transform.position.y);
     }
 
     IEnumerator Teleport(){
         playerMovement.SetDisabled(true);
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
 
         // Teleports the player
         var newpos = orientation.forward * teleportRange;
         newpos.y = 0;
+        audioSource.PlayOneShot(audioSource.clip, volume);
         transform.position += newpos;
         print("position : " + newpos + " y = " + newpos.y);
- 
         yield return new WaitForSeconds(0.1f);
         playerMovement.SetDisabled(false);
     }
