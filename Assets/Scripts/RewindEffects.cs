30d09fcca15745b14ff2207422bc4e7127197eb9
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-02-03 02:36:48 +0100

    Fixed rewind bug

---
+++ Assets/Scripts/RewindEffects.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kino;
+public class RewindEffects : MonoBehaviour
+{
+    public RewindTime rewindObj;
+    public int flashes = 5;
+    public float waitTime = 0.04f;    
+    public float modt = .002f;   
+    public float modWaitTime;
+    // sound / visual Effects
+    public GameObject spotLight;
+    public AudioSource audioSource;
+    public Color color1;
+    public Color color2;
+    public AnalogGlitch analogGlitch;
+    public float colorDrift = .5f;
+    public float scanLineJitter = .03f;
+    public float verticalJump = .03f;
+    public float horizontalShake = .1f;
+    bool isfirstTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isfirstTime = true;
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(rewindObj == null) {
+            // List<GameObject>[] g = GameObject.FindGameObjectsWithTag("TeleportableObjects");
+            // if(g.) {
+            //     rewindObj = g.GetComponent<RewindTime>();                
+            // }
+        }else{
+            if(rewindObj.isRewinding && isfirstTime){
+                audioSource.Play();
+                Glitch(colorDrift, scanLineJitter, verticalJump, horizontalShake);
+                StartCoroutine("LightEffects");
+                isfirstTime = false;
+            }else if (!isfirstTime && !rewindObj.isRewinding){
+                Glitch(0,0,0,0);
+                spotLight.SetActive(false);
+                audioSource.Stop();
+                isfirstTime = true;
+            }
+        }
+
+    }
+
+
+    
+    void Glitch(float colorDrift, float scanLineJitter,float verticalJump, float horizontalShake){
+        analogGlitch.colorDrift = colorDrift;
+        analogGlitch.scanLineJitter = scanLineJitter;
+        analogGlitch.verticalJump = verticalJump;
+        analogGlitch.horizontalShake = horizontalShake;
+    }
+
+    IEnumerator LightEffects(){
+        spotLight.SetActive(true);
+        modWaitTime = waitTime;
+        Light light = spotLight.GetComponent<Light>();
+        for(int i = 0; i < flashes; i++) {
+            light.color = color1;
+            yield return new WaitForSeconds(modWaitTime);
+            light.color = color2;
+            yield return new WaitForSeconds(modWaitTime);  
+            light.color = color1;
+            yield return new WaitForSeconds(modWaitTime);
+            light.color = color2;
+            yield return new WaitForSeconds(modWaitTime);
+            modWaitTime = (float) (modWaitTime + modt);
+        }
+    }
+
+}
--- Assets/Scripts/RewindTime.cs
+++ Assets/Scripts/RewindTime.cs
@@ -11,26 +11,11 @@ public class RewindTime : MonoBehaviour
     Vector3 origVelocity;
     bool isOrigVelocity = true;
     Rigidbody rb;
-    public int flashes = 5;
-    public float waitTime = 0.04f;    
-    public float modt = .002f;   
-    public float modWaitTime;
-    // sound / visual Effects
-    public GameObject spotLight;
-    AudioSource audioSource;
-    public Color color1;
-    public Color color2;
-    public AnalogGlitch analogGlitch;
-    public float colorDrift = .5f;
-    public float scanLineJitter = .03f;
-    public float verticalJump = .03f;
-    public float horizontalShake = .1f;
     // Start is called before the first frame update
     void Start()
     {
         rewindPoints = new ArrayList();
         rb = GetComponent<Rigidbody>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     void Update() {
@@ -49,7 +34,6 @@ public class RewindTime : MonoBehaviour
     void StopRewind(){
         isRewinding = false;
         rb.isKinematic = false;
-        spotLight.SetActive(false);
     }
 
     void FixedUpdate() {
@@ -71,12 +55,10 @@ public class RewindTime : MonoBehaviour
 
     void Rewind(){
 
-        Glitch(colorDrift, scanLineJitter, verticalJump, horizontalShake);
         if(isOrigVelocity) {
             PointsInTime p = (PointsInTime) rewindPoints[rewindPoints.Count - 1];
             origVelocity = p.GetVelocity();
             isOrigVelocity = false;
-            StartCoroutine("LightEffects");
         }
         if (rewindPoints.Count > 0){
             PointsInTime p = (PointsInTime) rewindPoints[0];
@@ -87,35 +69,8 @@ public class RewindTime : MonoBehaviour
             StopRewind();
             rb.velocity = origVelocity;
             isOrigVelocity = true;
-            Glitch(0,0,0,0);
-            audioSource.Stop();
         }
     
-    }
-
-    void Glitch(float colorDrift, float scanLineJitter,float verticalJump, float horizontalShake){
-        analogGlitch.colorDrift = colorDrift;
-        analogGlitch.scanLineJitter = scanLineJitter;
-        analogGlitch.verticalJump = verticalJump;
-        analogGlitch.horizontalShake = horizontalShake;
-    }
-
-    IEnumerator LightEffects(){
-        audioSource.Play();
-        spotLight.SetActive(true);
-        modWaitTime = waitTime;
-        Light light = spotLight.GetComponent<Light>();
-        for(int i = 0; i < flashes; i++) {
-            light.color = color1;
-            yield return new WaitForSeconds(modWaitTime);
-            light.color = color2;
-            yield return new WaitForSeconds(modWaitTime);  
-            light.color = color1;
-            yield return new WaitForSeconds(modWaitTime);
-            light.color = color2;
-            yield return new WaitForSeconds(modWaitTime);
-            modWaitTime = (float) (modWaitTime + modt);
-        }
     }
 
 
